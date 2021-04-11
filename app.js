f88eed2ca1e22de52d03cd39c6597eb2eb14a9ef
mukhtar <you@example.commukh-i1511@outlook.com>
2021-04-11 19:39:12 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,102 @@
+const navLists = document.querySelectorAll(".nav__list");
+const sections = document.querySelectorAll("section");
+const home = document.querySelector("#home");
+const navContainer = document.querySelector(".nav-container");
+const goToServicesBtn = document.querySelector('.btn-green[href="#services"]');
+const mobileNav = document.querySelector(".mobile-nav");
+const mobileMenu = document.querySelector(".mobile-menu");
+
+const goToSection = (e) => {
+  e.preventDefault();
+  if (e.target.tagName !== "A") return;
+  const linkHref = e.target.getAttribute("href").replace("#", "");
+  if (linkHref === home.id) home.scrollIntoView({ behavior: "smooth" });
+  sections.forEach((section) => {
+    if (linkHref === section.id) section.scrollIntoView({ behavior: "smooth" });
+  });
+  if (e.target.closest(".slide-up")) mobileNav.classList.remove("slide-up");
+};
+
+// smooth scroll
+navLists.forEach((navList) =>
+  navList.addEventListener("click", goToSection.bind())
+);
+
+// hamburger menu
+mobileMenu.addEventListener("click", () => {
+  mobileNav.classList.toggle("slide-up");
+});
+
+// sticky nav
+const navHeight = navContainer.getBoundingClientRect().height;
+
+const handleStickyNav = (entries) => {
+  const [entry] = entries;
+  entry.isIntersecting
+    ? navContainer.classList.remove("sticky")
+    : navContainer.classList.add("sticky");
+};
+
+const headerObserver = new IntersectionObserver(handleStickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
+});
+
+headerObserver.observe(home);
+
+goToServicesBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  document.querySelector("#services").scrollIntoView({ behavior: "smooth" });
+});
+
+// tabs
+const tabContainer = document.querySelector(".tabs");
+const tabBtns = document.querySelectorAll(".tabs__btn");
+const cards = document.querySelectorAll(".card");
+
+tabContainer.addEventListener("click", (e) => {
+  const target = e.target;
+  if (target.tagName !== "BUTTON") return;
+
+  tabBtns.forEach((tabBtn) => {
+    tabBtn.classList.remove("active");
+  });
+
+  target.classList.add("active");
+
+  cards.forEach((card) => {
+    card.classList.remove("show-card");
+  });
+
+  const tabNum = target.dataset.tab;
+
+  const cardToShow = document.querySelector(`.tabs__content-${tabNum}`);
+  cardToShow.classList.add("show-card");
+});
+
+// Testimonials
+const reviews = document.querySelectorAll(".review");
+
+reviews.forEach((review, index) => {
+  review.style.transform = `translateX(${100 * index}%)`;
+});
+
+let currentReview = 0;
+let firstReview = 0;
+const lastReview = reviews.length - 1;
+
+//showCurrentReview(); TODO
+
+// function showCurrentReview() {
+//   if (currentReview === lastReview) {
+//     currentReview = 0;
+//   } else {
+//     currentReview++;
+//   }
+
+//   reviews.forEach((review, index) => {
+//     review.style.transform = `translateX(${110 * (index - currentReview)}%)`;
+//   });
+//   setTimeout(showCurrentReview, 3000); // Change image every 2 seconds
+// }
